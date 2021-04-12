1c43389f40515343dded699a2d2783c76b583c2b
Meaghan Raleigh <meagblock@gmail.com>
2021-04-12 18:27:12 -0500

    Update css and JS, and profile image

--- js/scripts.js
+++ js/scripts.js
@@ -29,6 +29,21 @@
             }
         }
     });
+    // profile image JS 
+    $("#profileImage").click(function(e) {
+        $("#imageUpload").click();
+    });
+
+function fasterPreview( uploader ) {
+    if ( uploader.files && uploader.files[0] ){
+          $('#profileImage').attr('src', 
+             window.URL.createObjectURL(uploader.files[0]) );
+    }
+}
+
+$("#imageUpload").change(function(){
+    fasterPreview( this );
+});
 
     // Closes responsive menu when a scroll trigger link is clicked
     $(".js-scroll-trigger").click(function () {
